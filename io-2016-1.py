e85f3d0e813bdeda618b6621808bfe059d47b9a1
Mikhail <tikhonya97@mail.ru>
2016-09-10 00:11:50 +0300

    first commit

---
+++ io-2016-1.py
@@ -0,0 +1,13 @@
+
+first = int(input("Enter number: "))
+second = int(input("Enter power: "))
+result = 1
+i = 0
+
+while i < second :
+    i = i + 1
+    result = result * first
+
+print("Result: ", result)
+
+    
